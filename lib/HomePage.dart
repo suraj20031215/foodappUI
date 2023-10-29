4343153b6093f907cd87943c2d3a77e48e6442d1
suraj20031215 <suraj20031215@gmail.com>
2023-10-29 15:58:14 +0530

    Changes & Add features

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -5,35 +5,81 @@ import 'package:flutter_svg/flutter_svg.dart';
 class HomePage extends StatelessWidget{
   const HomePage({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
 
+      appBar: AppBar(
+        backgroundColor: Colors.purple[50],
+        title: Text('Food App UI'),
+        actions: [
+          PopupMenuButton(
+            // add icon, by default "3 dot" icon
+            // icon: Icon(Icons.book)
+              itemBuilder: (context){
+                return [
+                  PopupMenuItem<int>(
+                    value: 0,
+                    child: Text("Home"),
+                  ),
+
+                  PopupMenuItem<int>(
+                    value: 1,
+                    child: Text("Account"),
+                  ),
+
+                  PopupMenuItem<int>(
+                    value: 2,
+                    child: Text("Logout"),
+                  ),
+
+                ];
+              },
+
+          ),
+        ],
+      ),
+
+
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Restaurants',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_fire_department),
+            label: 'Offers',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+
+        ],
+      ),
+
       body:SingleChildScrollView(
+
         child: Column(
           children: [
-            Container(
-              margin: const EdgeInsets.only(top: 29),
-              child: const Text('Delivering to'),
-            ),
-            Container(
-              margin: const EdgeInsets.only(left: 18),
-              child: const Text('Current location',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-            ),
+
 
               Row(
                 children: [
                   const Padding(
-                    padding: EdgeInsets.all(8.0),
+                    padding: EdgeInsets.only(left:13,right: 10,bottom:7,top:10),
                     child: SizedBox(
-                      width: 340,
+                      width: 320,
                       height: 40,
                       child: TextField(
-                        decoration: InputDecoration(prefixIcon: Icon(Icons.search),border: OutlineInputBorder(borderSide:BorderSide(width: 1))),
+
+                        decoration: InputDecoration(prefixIcon: Icon(Icons.search),border: OutlineInputBorder(borderSide:BorderSide(width: 1)),hintText: 'Search here....'),
                       ),
                     ),
                   ),
-                  SvgPicture.asset('assets/images/menu-2-svgrepo-com.svg')
+                  SvgPicture.asset('assets/images/mic.svg',height: 35,)
                 ],
               ),
             SingleChildScrollView(
@@ -41,56 +87,238 @@ class HomePage extends StatelessWidget{
                 
                 child: Row(children: [
 
-                      Container(
-                        margin: const EdgeInsets.all(5),
-                        width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(color: Colors.red[50],borderRadius: const BorderRadius.all(Radius.circular(10))),
-                      child:SvgPicture.asset('assets/images/offer.svg')
+                      Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(left:10,right:5,top:5,bottom:5),
+                            width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(color: Colors.red[50],borderRadius: const BorderRadius.all(Radius.circular(10))),
+                          child:SvgPicture.asset('assets/images/offer.svg')
+                          ),
+                          Text('Offer',style: TextStyle(fontWeight: FontWeight.w600))
+                        ],
                       ),
 
-                  Container(
-                    margin: const EdgeInsets.all(5),
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(color: Colors.red[100],borderRadius: const BorderRadius.all(Radius.circular(10))),
-                  child:  SvgPicture.asset('assets/images/burger.svg'),
+                  Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(color: Colors.red[100],borderRadius: const BorderRadius.all(Radius.circular(10))),
+                      child:  SvgPicture.asset('assets/images/burger.svg'),
+                      ),
+                      Text('Burger',style: TextStyle(fontWeight: FontWeight.w600),)
+                    ],
                   ),
 
-                  Container(
-                    margin: const EdgeInsets.all(5),
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(color: Colors.tealAccent[100],borderRadius: const BorderRadius.all(Radius.circular(10))),
-                  child:  SvgPicture.asset('assets/images/pizza.svg'),
+                  Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(color: Colors.tealAccent[100],borderRadius: const BorderRadius.all(Radius.circular(10))),
+                      child:  SvgPicture.asset('assets/images/pizza.svg'),
+                      ),
+                      Text('Pizza',style: TextStyle(fontWeight: FontWeight.w600))
+                    ],
                   ),
 
-                  Container(
-                    margin: const EdgeInsets.all(5),
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(color: Colors.purple[100],borderRadius: const BorderRadius.all(Radius.circular(10))),
-                  child:  SvgPicture.asset('assets/images/fast-food-svgrepo-com.svg'),
+                  Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(color: Colors.purple[100],borderRadius: const BorderRadius.all(Radius.circular(10))),
+                      child:  SvgPicture.asset('assets/images/soft-drink.svg'),
+                      ),
+                      Text('Soft Drink',style: TextStyle(fontWeight: FontWeight.w600))
+                    ],
                   ),
 
-                  Container(
-                    margin: const EdgeInsets.all(5),
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(color: Colors.orange[100],borderRadius: const BorderRadius.all(Radius.circular(10))),
-                  child:  SvgPicture.asset('assets/images/menu-2-svgrepo-com.svg'),
+                  Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(color: Colors.orange[100],borderRadius: const BorderRadius.all(Radius.circular(10))),
+                      child:  SvgPicture.asset('assets/images/french.svg'),
+                      ),
+                      Text('French fry',style: TextStyle(fontWeight: FontWeight.w600))
+                    ],
                   ),
 
-                  Container(
-                    margin: const EdgeInsets.all(5),
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(color: Colors.green[100],borderRadius: const BorderRadius.all(Radius.circular(10))),
-                  child:  SvgPicture.asset('assets/images/menu-2-svg-repo-com.svg'),
+                  Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(5),
+                        width: 80,
+                        height: 80,
+                        decoration: BoxDecoration(color: Colors.green[100],borderRadius: const BorderRadius.all(Radius.circular(10))),
+                      child:  SvgPicture.asset('assets/images/spaghetti-pasta.svg'),
+                      ),
+                      Text('Pasta',style: TextStyle(fontWeight: FontWeight.w600))
+                    ],
                   ),
                 ],
                 ),
               ),
+            Container(
+              width: double.infinity,
+              height: 200,
+              margin: EdgeInsets.only(left: 10,right:10,top:10),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(10)),
+              child: Image.asset('assets/images/momos.png',fit:BoxFit.fill),
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('Veg Darjeeling Steam Momo',style: TextStyle(fontSize: 23,fontWeight: FontWeight.w700),),
+                ),
+              ],
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('Real Japanese Taste',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500),),
+                ),
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 200,
+              margin: EdgeInsets.only(left: 10,right:10,top:10),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+              child: Image.asset('assets/images/img_1.png',fit: BoxFit.fill),
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('Indian Food',style: TextStyle(fontSize: 23,fontWeight: FontWeight.w700),),
+                ),
+              ],
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('Real Indian Taste',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500),),
+                ),
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 200,
+              margin: EdgeInsets.only(left: 10,right:10,top:10),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+              child: Image.asset('assets/images/biryani.png',fit: BoxFit.fill,),
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('Lazeez Bhuna Murgh',style: TextStyle(fontSize: 23,fontWeight: FontWeight.w700),),
+                ),
+              ],
+            ),
+            const Row(
+
+              children: [
+                Padding(
+
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('(Boneless, Served )',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500),),
+                ),
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 200,
+              margin: EdgeInsets.only(left: 10,right:10,top:10),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+              child: Image.asset('assets/images/noodles.png',fit: BoxFit.fill),
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('Veggie Noodles',style: TextStyle(fontSize: 23,fontWeight: FontWeight.w700),),
+                ),
+              ],
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('only 230 rupess',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500),),
+                ),
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 200,
+              margin: EdgeInsets.only(left: 10,right:10,top:10),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+              child: Image.asset('assets/images/potato.png',fit: BoxFit.fill),
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('Cripsy Honey Potato',style: TextStyle(fontSize: 23,fontWeight: FontWeight.w700),),
+                ),
+              ],
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('Real Japanese Taste',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500),),
+                ),
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 200,
+              margin: EdgeInsets.only(left: 10,right:10,top:10),
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
+              child: Image.asset('assets/images/img_3.png',fit:BoxFit.fill),
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('Veggie Diet Salt & Pepper',style: TextStyle(fontSize: 23,fontWeight: FontWeight.w700),),
+                ),
+              ],
+            ),
+            const Row(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left:10),
+                  child: Text('Stay Healthy',style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500),),
+                ),
+              ],
+            ),
+
 
           ],
         ),
